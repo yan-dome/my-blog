117e1c6003fafc8a88a615c6e32e6465f2472c9b
yanshuaihao <webyanshuaihao@163.com>
2021-11-02 20:18:59 +0800

    第二天侧边栏

--- src/components/SiteAside/Blog/index.vue
+++ src/components/SiteAside/Blog/index.vue
@@ -17,15 +17,16 @@
 </template>
 
 <script>
-import Icon from "../Icon";
+import Icon from "@/components/Icon";
 export default {
   data() {
     return {
       items: [
         { title: "主页", type: "home", link: "/" },
-        { title: "微信", type: "weixin", link: "/weixin",statWideh:true },
-        { title: "gihub", type: "gihub", link: "/gihub" },
-        { title: "qq", type: "qq", link: "/qq" },
+        { title: "博客", type: "blog", link: "/blog",statWideh:true },
+        { title: "关于我", type: "about", link: "/about" },
+        { title: "项目&效果", type: "code", link: "/code" },
+        { title: "留言板", type: "chat", link: "/meagges" },
       ],
     };
   },
@@ -48,19 +49,22 @@ export default {
 </script>
 
 <style lang='less' scoped>
+@import "~@/styles/styles/var.less";
+
 .blog-container {
+  box-sizing: border-box;
   list-style: none;
   margin: 0;
-  padding: 20px;
+  padding: 0px;
 
   & li {
     width: 100%;
     height: 30px;
     line-height: 30px;
     box-sizing: border-box;
-    margin: 5px;
-    padding: 0 5px;
-    background-color: hotpink;
+    margin: 10px 0;
+    padding: 0px 5px;
+    background-color: inherit;
   }
   & a {
     display: flex;
@@ -69,13 +73,15 @@ export default {
     margin: 0 10px;
     align-items: center;
     cursor: pointer;
+    font-size: 22px;
   }
   .icon {
-    font-size: 20px;
-    margin: 0 5px;
+    font-size: 22px;
+    margin: 0 10px;
   }
   .active {
-    color: red;
+    background-color:lighten(@dark,10%) ;
+    color: #ccc;
   }
 }
 </style>
